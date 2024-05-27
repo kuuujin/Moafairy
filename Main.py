d66dccca571b549066518c9e368d40e6f08a7cb7
kuuujin <150771340+kuuujin@users.noreply.github.com>
2024-05-27 16:33:48 +0900

    modified:   Main.py 	modified:   __pycache__/Connectmodule.cpython-310.pyc

--- Main.py
+++ Main.py
@@ -1,149 +1,129 @@
 import pickle
 import sys
-import PyQt5.uic as uic
-from Connectmodule import ClientSocket
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow , QComboBox , QTextEdit
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import *
-import requests
+import threading
 import webbrowser
-
-
+import requests
+from Connectmodule import ClientSocket
+from PyQt5 import uic, QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QVBoxLayout, QStackedWidget
+from PyQt5 import QtWidgets
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 #서버와 연결
 Clientsocket = ClientSocket()
 Clientsocket.Connect()
 
 
-class Loginframe(QMainWindow):
+class LoginFrame(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi("Loginframe.ui",self)
-        self.Loginbtn.clicked.connect(self.login)
-        self.setMinimumSize(640, 480)
-        self.setMaximumSize(800, 600)
-        
-
-
-    def login(self):
-        webbrowser.open('http://35.216.101.141:5000/login')
+        self.ui = uic.loadUi("loginframe.ui", self)
+        self.ui.Loginbtn.clicked.connect(self.open_login_page)
     
+    def open_login_page(self):
+        # 외부 브라우저를 열어서 로그인 페이지로 이동
+        login_url = "http://35.216.101.141:5000/login"  # 로그인 페이지 URL 설정
+        webbrowser.open(login_url)
 
-#메인프레임
-class Mainframe(QMainWindow):
+# 메인 프레임
+class MainFrame(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi("Mainframe.ui",self)
+        self.ui = uic.loadUi("MainFrame.ui", self)
         self.Scanbtn.clicked.connect(self.Scanbtnclick)
         self.Searchbtn.clicked.connect(self.Searchbtnclick)
         self.show()
-        
-    #스캔프레임전환함수
-    def Scanbtnclick(self):
-        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def check_token_validity(self):
+        # 서버에서 로그인 토큰의 유효성을 확인하는 요청을 보냅니다.
+        token = self.get_saved_token()
+        if token:
+            response = requests.post("http://35.216.101.141:5000/verify_token", json={"token": token})
+            if response.status_code == 200:
+                # 토큰이 유효한 경우 메인 프레임으로 전환합니다.
+                widget.setCurrentIndex(widget.currentIndex() + 1)
+                # 토큰 확인을 더 이상 반복하지 않고 중지합니다.
+                self.stop_token_checking()
+
+    def stop_token_checking(self):
+        # 토큰 확인 작업을 중지합니다.
+        self.token_checking_timer.cancel()
     
-    #검색프레임전환함수
+    def get_saved_token(self):
+        # 클라이언트에서 저장된 로그인 토큰을 반환합니다.
+        # 여기서는 임의의 값인 "saved_token"을 반환하도록 설정합니다.
+        return "saved_token"
+
+    def Scanbtnclick(self):
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
     def Searchbtnclick(self):
-        widget.setCurrentIndex(widget.currentIndex()+2)
+        widget.setCurrentIndex(widget.currentIndex() + 2)
 
-
-#스캔프레임
-class Scanframe(QMainWindow):
+# 스캔 프레임
+class ScanFrame(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi("Scanframe.ui",self)
-        
-        #이벤트 함수
-        #self.변수명.clicked.connect(self.함수)
-        
-        #콤보박스
-        self.Combo_category=QComboBox(self)
-        self.Combo_category.addItems(['전체','의류','음식','전자','서적','게임'])
+        self.ui = uic.loadUi("ScanFrame.ui", self)
+        self.Combo_category = QComboBox(self)
+        self.Combo_category.addItems(['전체', '의류', '음식', '전자', '서적', '게임'])
         self.Combo_category.setStyleSheet("background-color:white")
-        self.Combo_category.setGeometry(120,210,91,35)
-        #키워드
-
-        #스캔프레임 변수
+        self.Combo_category.setGeometry(120, 210, 91, 35)
         self.Sendbtn.clicked.connect(self.Sendserver)
         self.Homebtn.clicked.connect(self.Homebtnclick)
 
-
-
-
-#메인프레임으로 이동 함수
     def Homebtnclick(self):
-        widget.setCurrentIndex(widget.currentIndex()-1)
+        widget.setCurrentIndex(widget.currentIndex() - 1)
 
-    #서버로전송함수
     def Sendserver(self):
         Keyword = self.Keyword.toPlainText()
-        Category= self.Combo_category.currentText()
-        Msg_source='scan'
-        
-        Data = pickle.dumps((Msg_source,Keyword,Category))
+        Category = self.Combo_category.currentText()
+        Data = pickle.dumps((Keyword, Category))
         Clientsocket = getattr(sys.modules[__name__], "Clientsocket")
         Clientsocket.Send(Data)
 
-#검색프레임(스캔프레임 상속으로 코드 간결화)
-class Searchframe(Scanframe):
+# 검색 프레임
+class SearchFrame(ScanFrame):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi("Searchframe.ui", self)
-
+        self.ui = uic.loadUi("SearchFrame.ui", self)
         self.Sendbtn.clicked.connect(self.Sendserver)
         self.Homebtn.clicked.connect(self.Homebtnclick)
-        self.Combo_category=QComboBox(self)
-        self.Combo_category.addItems(['전체','의류','음식','전자','서적','게임'])
+        self.Combo_category = QComboBox(self)
+        self.Combo_category.addItems(['전체', '의류', '음식', '전자', '서적', '게임'])
         self.Combo_category.setStyleSheet("background-color:white")
-        self.Combo_category.setGeometry(120,210,91,35)
-    # 메인프레임으로 이동 함수 (재정의)
+        self.Combo_category.setGeometry(120, 210, 91, 35)
+
     def Homebtnclick(self):
         widget.setCurrentIndex(widget.currentIndex() - 2)
-    
-
-    #서버로전송함수
-    def Sendserver(self):
-        Keyword = self.Keyword.toPlainText()
-        Category= self.Combo_category.currentText()
-        Msg_source='search'
-        
-        Data = pickle.dumps((Msg_source,Keyword,Category))
-        Clientsocket = getattr(sys.modules[__name__], "Clientsocket")
-        Clientsocket.Send(Data)
-
 
 if __name__ == "__main__":
-    
     app = QApplication(sys.argv)
-    #app.setStyleSheet("QWidget { background-color: #e6e6fa; }")
-    #화면 전환용 widget
     widget = QtWidgets.QStackedWidget()
 
-    #레이아웃 인스턴스
-    Loginframelayout = Loginframe()
-    Mainframelayout = Mainframe()
-    Scanframelayout = Scanframe()
-    Searchframelayout = Searchframe()
+    login_frame = LoginFrame()
+    main_frame = MainFrame()
+    scan_frame = ScanFrame()
+    search_frame = SearchFrame()
 
-    #위젯에 추가
-    widget.addWidget(Loginframelayout)
-    widget.addWidget(Mainframelayout)
-    widget.addWidget(Scanframelayout)
-    widget.addWidget(Searchframelayout)
+    widget.addWidget(login_frame)
+    widget.addWidget(main_frame)
+    widget.addWidget(scan_frame)
+    widget.addWidget(search_frame)
     widget.setFixedWidth(800)
     widget.setFixedHeight(1000)
+
+    main_frame.token_checking_timer = threading.Timer(20.0, main_frame.check_token_validity)
+    main_frame.token_checking_timer.start()
+
     widget.show()
+
     def Close_socket():
         if Clientsocket:
             Clientsocket.Disconnect()
-        
+
     app.aboutToQuit.connect(Close_socket)
-    #프로그램 실행
     app.exec_()
-
-
-
 
 
 
